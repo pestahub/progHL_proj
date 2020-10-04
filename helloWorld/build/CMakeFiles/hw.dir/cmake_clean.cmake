9eb44a7c3daba61cd96e928416e1ed597f2b9fd0
pesta <andru.pestov@gmail.com>
2020-10-04 14:27:35 +0300

    add hw and quiz

---
+++ helloWorld/build/CMakeFiles/hw.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw.dir/hw.cpp.o"
+  "hw.pdb"
+  "hw"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
